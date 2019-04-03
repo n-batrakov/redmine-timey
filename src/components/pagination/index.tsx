f0206c6960d9a54d052bc49ea5eefe8df084f731
Никита Батраков <n.batrakov@itexpert.ru>
2019-04-03 09:53:09 +0300

    Fix pagination `count` behaviour

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -4,15 +4,15 @@ import './pagination.css';
 
 export type PaginationProps = {
     count: number,
+    pageSize: number,
     currentPage?: number,
-    pageSize?: number,
     onSelect?: (page: number) => void,
 };
 export const Pagination = (props: PaginationProps) => (
     <div style={{display: 'flex'}}>
     <ReactPaginate
         containerClassName="timey-pagination"
-        pageCount={props.count}
+        pageCount={Math.ceil(props.count / props.pageSize)}
         pageRangeDisplayed={3}
         marginPagesDisplayed={3}
         initialPage={props.currentPage}
