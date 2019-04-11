f41bd974e35909b2d14aeb8d5285bec7f456ce15
Никита Батраков <n.batrakov@itexpert.ru>
2019-04-11 15:15:25 +0300

    Add Icons components

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+export const IconAdd = () => (<FontAwesomeIcon icon={faPlus} />);
+export const IconBack = () => (<FontAwesomeIcon icon={faArrowLeft} />);
