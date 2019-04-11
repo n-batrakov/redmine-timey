84134203ae61ed16b3130ba0e32de40845e4a78b
Никита Батраков <n.batrakov@itexpert.ru>
2019-04-11 15:00:36 +0300

    Extend Button component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -4,22 +4,37 @@ import { assertNever } from '../../shared';
 import './form.css';
 import { Preloader } from '../preloader';
 
-const getBtnTypeClass = ({ type }: ButtonProps) => {
-    if (type === undefined) {
-        return 'btn';
-    }
+const getBtnClass = ({ type, size }: ButtonProps) => {
+    const getTypeClass = () => {
+        switch (type) {
+            case undefined:
+            case 'default':
+                return '';
+            case 'primary':
+            case 'submit':
+                return 'btn-primary';
+            case 'danger':
+                return 'btn-danger';
+            default:
+                assertNever(type);
+                return '';
+        }
+    };
 
-    switch (type) {
-        case 'default':
-            return 'btn';
-        case 'primary':
-        case 'submit':
-            return 'btn btn-primary';
-        case 'danger':
-            return 'btn btn-danger';
-        default:
-            assertNever(type);
-    }
+    const getSizeClass = () => {
+        switch(size) {
+            case undefined:
+            case 'default':
+                return '';
+            case 'small':
+                return 'btn-small';
+            default:
+                assertNever(size);
+                return '';
+        }
+    };
+
+    return `btn ${getTypeClass()} ${getSizeClass()}`;
 };
 
 type FormControlProps = {
@@ -31,14 +46,16 @@ type FormControlProps = {
 };
 
 export type ButtonProps = {
-    value: string,
+    value: React.ReactNode,
     onClick?: () => void,
     type?: 'default' | 'primary' | 'danger' | 'submit',
+    size?: 'small' | 'default',
     style?: React.CSSProperties,
+    tooltip?: string,
 };
 export const Button = (x: ButtonProps) => (
     <button
-        className={getBtnTypeClass(x)}
+        className={getBtnClass(x)}
         type={x.type === 'submit' ? 'submit' : 'button'}
         onClick={x.onClick}
         style={x.style}
