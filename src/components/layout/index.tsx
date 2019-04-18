06d3b98fa304ed448f7b6ec12a521322c18b351a
n.batrakov <n.m.batrakov@gmail.com>
2019-04-18 16:23:14 +0300

    Export Layout prop types

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,8 +1,16 @@
 import '../../../node_modules/react-flexbox-grid/dist/react-flexbox-grid.css';
-import { Grid as BaseGrid, Row as BaseRow, Col as BaseCol, ColProps } from 'react-flexbox-grid';
+import * as React from 'react';
+import {
+    Grid as BaseGrid, GridProps as BaseGridProps,
+    Row as BaseRow, RowProps as BaseRowProps,
+    Col as BaseCol, ColProps as BaseColProps,
+} from 'react-flexbox-grid';
 
-export const Grid = BaseGrid;
+export const Grid = (props: GridProps) => <BaseGrid fluid {...props} />;
+export type GridProps = BaseGridProps;
 
 export const Row = BaseRow;
+export type RowProps = BaseRowProps;
 
 export const Col = BaseCol;
+export type ColProps = BaseColProps;
