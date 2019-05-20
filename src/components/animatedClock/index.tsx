69e805ec27c34161e37f79819cd56565573d9730
n.batrakov <n.m.batrakov@gmail.com>
2019-05-20 11:08:12 +0300

    Add clock loading mode

--- src/components/animatedClock/index.tsx
+++ src/components/animatedClock/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 export type AnimatedClockProps = {
     date: Date,
+    loading?: boolean,
     style?: React.CSSProperties,
 };
 
@@ -10,14 +11,17 @@ export const AnimatedClock = (props: AnimatedClockProps) => {
 
     const seconds = date.getSeconds();
     const secondsAngle = 360 * (seconds / 60);
+    const secondsDuration = props.loading ? '0.5s' : '60s';
 
     const minutes = date.getMinutes();
     const minutesValue = (minutes * 60) + seconds;
     const minutesAngle = 360 * (minutesValue / 3600);
+    const minuteDuration = props.loading ? '1s' : '3600s';
 
     const hours = date.getHours();
     const hoursValue = ((hours > 12 ? hours - 12 : hours) * 3600) + minutesValue;
     const hoursAngle = 360 * (hoursValue / 43200);
+    const hoursDuration = props.loading ? '1.5s' : '43200s';
 
     return (
         <svg
@@ -69,7 +73,7 @@ export const AnimatedClock = (props: AnimatedClockProps) => {
                     additive="sum"
                     xlinkHref="#timey-clock-hour-hand"
                     repeatCount="indefinite"
-                    dur="43200s"
+                    dur={hoursDuration}
                     to="360 192 192"
                     from="0 192 192"
                     attributeName="transform"
@@ -84,7 +88,7 @@ export const AnimatedClock = (props: AnimatedClockProps) => {
                     additive="sum"
                     xlinkHref="#timey-clock-minute-hand"
                     repeatCount="indefinite"
-                    dur="3600s"
+                    dur={minuteDuration}
                     to="360 192 192"
                     from="0 192 192"
                     attributeName="transform"
@@ -99,7 +103,7 @@ export const AnimatedClock = (props: AnimatedClockProps) => {
                     additive="sum"
                     xlinkHref="#timey-clock-second-hand"
                     repeatCount="indefinite"
-                    dur="60s"
+                    dur={secondsDuration}
                     to="360 192 192"
                     from="0 192 192"
                     attributeName="transform"
