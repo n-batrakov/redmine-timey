1f6d19c2b69fe24c90789dde21e961570bd46bdb
n.batrakov <n.m.batrakov@gmail.com>
2019-05-21 14:19:19 +0300

    Fix session api

--- src/api/getSession/handler.ts
+++ src/api/getSession/handler.ts
@@ -1,9 +1,10 @@
 import { metadata, GetSessionResponse } from './contract';
 import { RegisterHandler } from '../../server/shared';
-import { getCredentials } from '../../server/auth';
+import { getCredentials, authenticate } from '../../server/auth';
 
 const handler: RegisterHandler = (server, { }) => server.route({
     ...metadata,
+    preHandler: authenticate,
     handler: async (req): Promise<GetSessionResponse> => {
         const { login } = getCredentials(req);
 
