76ddfee39db35344695039512a929a488eb99b5b
n.batrakov <n.m.batrakov@gmail.com>
2019-08-20 11:00:59 +0300

    Add alert component

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,21 @@
+import './index.css';
+import React from 'react';
+
+export type AlertProps = { children?: React.ReactNode };
+
+const Alert = (props: AlertProps & { type: 'success' | 'info' | 'warning' | 'danger' }) => {
+    if (props.children === undefined) {
+        return null;
+    }
+
+    return (
+        <div className={`alert ${props.type || 'info'}`}>
+            {props.children}
+        </div>
+    );
+};
+
+export const Success = (props: AlertProps) => <Alert type="success" {...props} />;
+export const Info = (props: AlertProps) => <Alert type="info" {...props} />;
+export const Warning = (props: AlertProps) => <Alert type="warning" {...props} />;
+export const Danger = (props: AlertProps) => <Alert type="danger" {...props} />;
