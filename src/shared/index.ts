717a4b7b6b0942d4ff16ce1fea276bdc96d09526
n.batrakov <n.m.batrakov@gmail.com>
2019-08-23 15:15:53 +0300

    Add isNullOrUndefined type guard

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -14,3 +14,11 @@ export const bind = (fn: ((...args: any[]) => any) | undefined, ...args: any[]) 
 };
 
 export const wait = (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
+
+export function isNullOrUndefined<T>(value: T | null | undefined): value is null | undefined {
+    return value === null || value === undefined;
+}
+
+export function notNullOrUndefined<T>(value: T | null |undefined): value is T {
+    return value !== undefined && value !== null;
+}
