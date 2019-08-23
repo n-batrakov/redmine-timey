a36b51d6ab79f4645e2fc5fac180571fb7c32113
n.batrakov <n.m.batrakov@gmail.com>
2019-08-23 09:48:15 +0300

    Refactor timing page

--- src/containers/editTimingPage.tsx
+++ src/containers/editTimingPage.tsx
@@ -3,218 +3,153 @@ import { Container } from '../components/container';
 import { RouteComponentProps } from 'react-router';
 import { tryParseDate } from '../shared/date';
 import { TimingForm } from '../components/timingForm';
-import { connect } from 'react-redux';
-import { AppState } from '../store';
+import { useSelector } from 'react-redux';
+import { AppState, useAppState } from '../store';
 import { MobileScreen, MobileScreenHidden } from '../components/mediaQuery';
 import { Tabs, TabList, TabPanel, Tab } from '../components/tabs';
-import { Issues } from './issues';
-import { EnumerationsState } from '../store/enumerations/types';
-import { Issue, TimesheetEntry } from '../shared/types';
+import { Issues as IssuesList } from './issues';
+import { Issue, Enumeration } from '../shared/types';
 import { ToggledIssueFilter, IssueFilterForm, OverflowIssueFilter } from '../components/issueFilter';
 import { applyFilter } from '../store/issues/actions';
-import { IssueFilterValue } from '../store/issues/types';
 import { addTimesheetEntry, updateTimesheetEntry, deleteTimesheetEntry, loadTimesheetEntry, selectIssue } from '../store/timingsForm/actions';
 import { Danger, Success } from '../components/alert';
+import { useBind } from '../store/useBind';
 
-const onTabChange = (props: { selectIssue?: (x?: string) => void }) => React.useCallback(
-    () => props.selectIssue === undefined ? undefined : props.selectIssue(undefined),
-    [props.selectIssue],
-);
-
-const onSelectIssue = (props: { selectIssue?: (x?: string) => void }) => React.useCallback(
-    (issue: Issue) => props.selectIssue === undefined ? undefined : props.selectIssue(issue.id),
-    [props.selectIssue],
-);
-
-const onDeleteTiming = (props: EditTimingPageProps)  => React.useCallback(
-    (id: string) => {
-        props.deleteTimesheetEntry(id);
-        props.history.replace('new');
-    },
-    [],
-);
-
-type PageProps = RouteComponentProps & {
-    loading?: boolean,
-    error?: string,
-    success?: boolean,
-    enumerations: EnumerationsState,
-
-    selectedIssueId?: string,
-    selectIssue?: (id?: string) => void,
-
-    entry?: Partial<TimesheetEntry>,
-    onSubmit?: (form: Partial<TimesheetEntry>) => void,
-    applyFilter?: (filter?: IssueFilterValue) => void,
-    onDelete?: (id: string) => void,
+type PageLayoutProps = {
+    filter?: React.ReactNode,
+    issues?: React.ReactNode,
+    form?: React.ReactNode,
 };
-const Page = ({ selectedIssueId, ...props }: PageProps) => {
-    const enums = props.enumerations;
-    const onSelect = onSelectIssue(props);
+const PageLayout = React.memo(
+    (props: PageLayoutProps) => {
+        const selectedIssueId = useAppState(x => x.timingsForm.selectedIssueId);
+        const unselectIssue = useBind(() => selectIssue.bind(undefined, undefined));
 
-    const form = (
-        <>
-            <Danger>{props.error}</Danger>
-            <Success>{props.success ? 'Success' : undefined}</Success>
-            <TimingForm
-                data={props.entry}
-                activities={enums.activity}
-                onClose={props.history.goBack}
-                onSubmit={props.onSubmit}
-                loading={props.loading}
-                disabled={selectedIssueId === undefined}
-                showDelete={props.onDelete !== undefined}
-                onDelete={props.onDelete}
-            />
-        </>
-    );
-
-    return (
-        <>
-            <MobileScreen>
-                <Container>
-                    <Tabs selectedIndex={selectedIssueId === undefined ? 0 : 1} onSelect={onTabChange(props)}>
-                        <TabList>
-                            <Tab>Issue</Tab>
-                            <Tab disabled={selectedIssueId === undefined}>Timing</Tab>
-                        </TabList>
-                        <TabPanel>
-                            <ToggledIssueFilter>
-                                <IssueFilterForm enums={enums} onSubmit={props.applyFilter} />
-                            </ToggledIssueFilter>
-                            <Issues onSelect={onSelect} selectedIssueId={selectedIssueId} />
-                        </TabPanel>
-                        <TabPanel>{form}</TabPanel>
-                    </Tabs>
-                </Container>
-            </MobileScreen>
-            <MobileScreenHidden>
-                <div style={{ display: 'flex' }}>
-                    <OverflowIssueFilter>
-                        <IssueFilterForm enums={enums} onSubmit={props.applyFilter} />
-                    </OverflowIssueFilter>
-                    <Container inline>
-                        <Issues
-                            onSelect={onSelect}
-                            selectedIssueId={selectedIssueId}
-                            style={{
-                                maxWidth: '60%',
-                                height: 'calc(100vh - 2*24px - 38px)',
-                                overflowX: 'auto',
-                                paddingRight: 10,
-                            }}
-                        />
-                        <div style={{ width: 500, padding: '0 20px' }}>
-                            {form}
-                        </div>
+        return (
+            <>
+                <MobileScreen>
+                    <Container>
+                        <Tabs selectedIndex={selectedIssueId === undefined ? 0 : 1} onSelect={unselectIssue}>
+                            <TabList>
+                                <Tab>Issue</Tab>
+                                <Tab disabled={selectedIssueId === undefined}>Timing</Tab>
+                            </TabList>
+                            <TabPanel>
+                                <ToggledIssueFilter>
+                                    {props.filter}
+                                </ToggledIssueFilter>
+                                {props.issues}
+                            </TabPanel>
+                            <TabPanel>
+                                {props.form}
+                            </TabPanel>
+                        </Tabs>
                     </Container>
-                </div>
-            </MobileScreenHidden>
-        </>
-    );
+                </MobileScreen>
+                <MobileScreenHidden>
+                    <div style={{ display: 'flex' }}>
+                        <OverflowIssueFilter>
+                            {props.filter}
+                        </OverflowIssueFilter>
+                        <Container inline>
+                            <div style={{
+                                    height: 'calc(100vh - 2*24px - 38px)',
+                                    overflowX: 'auto',
+                                    paddingRight: 10,
+                                    marginRight: 20,
+                                    flex: '1 1 auto',
+                                }}
+                            >
+                                {props.issues}
+                            </div>
+                            <div style={{ width: 300 }}>
+                                {props.form}
+                            </div>
+                        </Container>
+                    </div>
+                </MobileScreenHidden>
+            </>
+        );
+    },
+    () => false,
+);
+
+
+const Filter = () => {
+    const enums = useAppState(x => x.enumerations);
+    const onSubmit = useBind(applyFilter);
+    return <IssueFilterForm enums={enums} onSubmit={onSubmit} />;
 };
 
+const Issues = () => {
+    const selectedIssueId = useAppState(x => x.timingsForm.selectedIssueId);
+    const onSelect = useBind((x: Issue) => selectIssue(x.id));
 
-export type EditTimingPageProps = RouteComponentProps<{id: string}> & {
-    loading: boolean,
-    error: string,
-    success: boolean,
-    entry?: TimesheetEntry,
-    enumerations: EnumerationsState,
-    applyFilter: (filter?: IssueFilterValue) => void,
-    loadTimesheetEntry: (id: string) => void,
-    updateTimesheetEntry: (entry: Partial<TimesheetEntry>) => void,
-    deleteTimesheetEntry: (id: string) => void,
-    selectedIssueId?: string,
-    selectIssue: (id?: string) => void,
+    return <IssuesList onSelect={onSelect} selectedIssueId={selectedIssueId} />;
 };
 
-const EditTimingContainer = (props: EditTimingPageProps) => {
-    const id = props.match.params.id;
-    React.useEffect(() => { props.loadTimesheetEntry(id); }, []);
-    const onDelete = onDeleteTiming(props);
+function getDateQueryParam() {
+    const params = new URLSearchParams(window.location.search);
+    const dateParam = params.get('date');
+    return dateParam === null ? undefined : tryParseDate(dateParam);
+}
 
-    if (props.entry === undefined) {
+type FormProps = { onCancel: () => void };
+
+const Form = (props: FormProps) => {
+    const state = useAppState(x => x.timingsForm);
+    const activities = useSelector<AppState, Enumeration>(x => x.enumerations.activity);
+    const actions = useBind({ addTimesheetEntry, updateTimesheetEntry, deleteTimesheetEntry });
+
+    if (state.loading) {
         return null;
     }
 
-    const entryIssue = props.entry.issue === undefined ? undefined : props.entry.issue.id;
-    const selectedIssueId = props.selectedIssueId || entryIssue;
+    const isCreate = state.entry === undefined;
+    const data = isCreate
+        ? {
+            spentOn: getDateQueryParam(),
+            issue: state.selectedIssueId === undefined ? undefined : { id: state.selectedIssueId },
+        }
+        : state.entry;
 
     return (
-        <Page
-            {...props}
-            selectedIssueId={selectedIssueId}
-            selectIssue={props.selectIssue}
-            onSubmit={props.updateTimesheetEntry}
-            onDelete={onDelete}
-        />
-    );
-};
-export const EditTimingPage = connect(
-    (state: AppState, props: Partial<EditTimingPageProps>): Partial<EditTimingPageProps> => ({
-        ...props,
-        enumerations: state.enumerations,
-        error: state.timingsForm.error,
-        loading: state.timingsForm.loading,
-        success: state.timingsForm.success,
-        entry: state.timingsForm.entry,
-        selectedIssueId: state.timingsForm.selectedIssueId,
-    }),
-    {
-        applyFilter,
-        updateTimesheetEntry,
-        deleteTimesheetEntry,
-        loadTimesheetEntry,
-        selectIssue,
-    },
-)(EditTimingContainer);
+        <>
+            <Danger>{state.error}</Danger>
+            <Success>{state.success ? 'Success' : undefined}</Success>
+            <TimingForm
+                disabled={state.selectedIssueId === undefined}
+                loading={state.loading}
+                data={data}
+                activities={activities}
+                submitLabel={isCreate ? 'Add' : 'Update'}
+                onSubmit={isCreate ? actions.addTimesheetEntry : actions.updateTimesheetEntry}
+                onClose={props.onCancel}
 
-
-
-export type CreateTimingPageProps = RouteComponentProps<{id: string}> & {
-    loading: boolean,
-    error: string,
-    success: boolean,
-    enumerations: EnumerationsState,
-    applyFilter: (filter?: IssueFilterValue) => void,
-    addTimesheetEntry: (e: TimesheetEntry) => void,
-    selectedIssueId?: string,
-    selectIssue: (id?: string) => void,
-};
-
-const CreatePage = (props: CreateTimingPageProps) => {
-    const params = new URLSearchParams(props.location.search);
-    const dateParam = params.get('date');
-    const spentOn = dateParam === null ? undefined : tryParseDate(dateParam);
-    const entry: Partial<TimesheetEntry> = props.selectedIssueId === undefined
-        ? { spentOn }
-        : { spentOn, issue: { id: props.selectedIssueId } };
-
-    return (
-        <Page
-            {...props}
-            entry={entry}
-            onSubmit={props.addTimesheetEntry}
-            selectedIssueId={props.selectedIssueId}
-            selectIssue={props.selectIssue}
-        />
+                showDelete={!isCreate}
+                onDelete={actions.deleteTimesheetEntry}
+            />
+        </>
     );
 };
 
-export const CreateTimingPage = connect(
-    (state: AppState, props: Partial<CreateTimingPageProps>): Partial<CreateTimingPageProps> => ({
-        ...props,
-        enumerations: state.enumerations,
-        error: state.timingsForm.error,
-        loading: state.timingsForm.loading,
-        success: state.timingsForm.success,
-        selectedIssueId: state.timingsForm.selectedIssueId,
-    }),
-    {
-        applyFilter,
-        addTimesheetEntry,
-        loadTimesheetEntry,
-        selectIssue,
-    },
-)(CreatePage);
+export const TimingPage = (props: RouteComponentProps<{ id: string }>) => {
+    const id = props.match.params.id;
+    const editMode = id !== 'new';
+
+    const loadEntry = useBind(loadTimesheetEntry);
+    React.useEffect(
+        () => {
+            if (editMode) loadEntry(id);
+        },
+        [id],
+    );
+
+    return (
+        <PageLayout
+            filter={<Filter />}
+            issues={<Issues />}
+            form={<Form onCancel={() => props.history.push('/')}/>}
+        />
+    );
+};
