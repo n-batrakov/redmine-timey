ec5324b57686d41d30aacf2ab1d9af2d86a67df2
n.batrakov <n.m.batrakov@gmail.com>
2019-09-06 11:28:33 +0300

    Cleanup timing page layout

--- src/features/timeEntry/components/layout/index.tsx
+++ src/features/timeEntry/components/layout/index.tsx
@@ -15,43 +15,47 @@ type PageLayoutProps = {
     onRefresh?: () => void,
 };
 
-const MobileLayout = (props: PageLayoutProps) => (
-    <main className="timing-page mobile">
-        <h1>{props.title}</h1>
-        <Tabs selectedIndex={props.issueSelected ? 1 : 0} onSelect={props.unselectIssue}>
-            <TabList>
-                <Tab>Issue</Tab>
-                <Tab disabled={props.issueSelected !== true}>Details</Tab>
-            </TabList>
-            <TabPanel>
-                <h3>Select an Issue</h3>
-                <ToggledIssueFilter>{props.filter}</ToggledIssueFilter>
-                {props.issues}
-            </TabPanel>
-            <TabPanel>
-                {props.form}
-            </TabPanel>
-        </Tabs>
-    </main>
-);
-
-const DesktopLayout = (props: PageLayoutProps) => (
-    <main className="timing-page desktop">
-        <OverflowIssueFilter opened={false} onRefresh={props.onRefresh}>
-            {props.filter}
-        </OverflowIssueFilter>
-        <div className="col">
+const MobileLayout = (props: PageLayoutProps) => {
+    return (
+        <main className="timing-page mobile">
             <h1>{props.title}</h1>
-            <div className="row">
-                <div className="issues-container">
+            <Tabs selectedIndex={props.issueSelected ? 1 : 0} onSelect={props.unselectIssue}>
+                <TabList>
+                    <Tab>Issue</Tab>
+                    <Tab disabled={props.issueSelected !== true}>Details</Tab>
+                </TabList>
+                <TabPanel>
                     <h3>Select an Issue</h3>
+                    <ToggledIssueFilter>{props.filter}</ToggledIssueFilter>
                     {props.issues}
+                </TabPanel>
+                <TabPanel>
+                    {props.form}
+                </TabPanel>
+            </Tabs>
+        </main>
+    );
+};
+
+const DesktopLayout = (props: PageLayoutProps) => {
+    return (
+        <main className="timing-page desktop">
+            <OverflowIssueFilter opened={false} onRefresh={props.onRefresh}>
+                {props.filter}
+            </OverflowIssueFilter>
+            <div className="col">
+                <h1>{props.title}</h1>
+                <div className="row">
+                    <div className="issues-container">
+                        <h3>Select an Issue</h3>
+                        {props.issues}
+                    </div>
+                    <div className="entry-container">{props.form}</div>
                 </div>
-                <div className="entry-container">{props.form}</div>
             </div>
-        </div>
-    </main>
-);
+        </main>
+    );
+};
 
 export const PageLayout = (props: PageLayoutProps) => (
     <>
