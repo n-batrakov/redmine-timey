9063f974741b2eea47d0823acc6e866f6ab5a2e9
n.batrakov <n.m.batrakov@gmail.com>
2019-09-23 10:16:27 +0300

    Use tablet breakpoint for mobile screen layout

--- src/components/mediaQuery/index.tsx
+++ src/components/mediaQuery/index.tsx
@@ -2,7 +2,7 @@ import MediaQuery from 'react-responsive';
 import * as React from 'react';
 
 export const MobileScreen = ({ children }: { children: React.ReactNode }) =>
-    <MediaQuery query="(max-width: 767px)">{children}</MediaQuery>;
+    <MediaQuery query="(max-width: 991.98px)">{children}</MediaQuery>;
 
 export const MobileScreenHidden = ({ children }: { children: React.ReactNode }) =>
-    <MediaQuery query="(min-width: 768px)">{children}</MediaQuery>;
+    <MediaQuery query="(min-width: 992px)">{children}</MediaQuery>;
